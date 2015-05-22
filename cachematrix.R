d2ee6cc22f0cf7ceb5c96ffafec78e755b62d8ec
Tyler Woods <tyler.woods@gmail.com>
2015-05-21 22:08:22 -0400

    Implemented routines for makeCacheMatrix and cacheSolve.  These routines closely mirror the suggested format within Programming Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions implement a simple cache for computationally
+## expensive matrix calculations.
 
-## Write a short comment describing this function
+## makeCacheMatrix constructs a list of functions that can be
+## used to access and set both the cached matrix and matrix inverse
+## This function is consumed by cacheSolve below.
+makeCacheMatrix <- function(cacheMatrix = matrix()) 
+{
+	cacheMatrixInverse <- NULL
 
-makeCacheMatrix <- function(x = matrix()) {
+	#Mutator to set the internal matrix
+	setMatrix <- function(newCacheMatrix)
+	{
+		cacheMatrix <<- newCacheMatrix
+		cacheMatrixInverse <<- NULL
+	}
+	#Accessor to retrieve the internal matrix
+	getMatrix <- function() 
+	
+	{
+		cacheMatrix
+	}
 
+	#Returned the cached inverse of X
+	getMatrixInverse <- function()
+	{
+		cacheMatrixInverse
+	}
+
+	#Set the cached inverse of X
+	setMatrixInverse <- function(newCacheMatrixInverse)
+	{
+		cacheMatrixInverse <<- newCacheMatrixInverse
+	}
+
+	list(set = setMatrix, get = getMatrix, 
+		 getInverse = getMatrixInverse, 
+		 setInverse = setMatrixInverse)
 }
 
 
-## Write a short comment describing this function
+##Retrieve the matrix inverse of the matrix stored within cacheMatrix.
+##Either retrieve the already computed value, or compute and set the
+##cached value accordingly.
+cacheSolve <- function(cacheMatrix, ...) 
+{
+    ## Check to see if the inverse is already calculated
+    inverse <- cacheMatrix$getInverse()
+    if(!is.null(inverse))
+    {
+    	#Found it!
+    	return(inverse)
+    }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    #No cached copy available.  Solve now and pass any
+    #parameters to the solver routine.  
+
+    #If we were being clever, we'd also detect to see if 
+    #the parameters passed to us were different than what 
+    #were originally used to generated our cached matrix 
+    #inverse.  But we're not clever, now are we?
+    inverse <- solve(cacheMatrix$get(), ...)
+    
+    #Cache the copy
+    cacheMatrix$setInverse(inverse)
+
+    #Return the newly computed inverse
+    return(inverse)
 }
